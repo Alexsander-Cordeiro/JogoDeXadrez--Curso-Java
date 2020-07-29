3e49b6f22d44af148811f4342bb56d1dada018c8
Alexsander-Cordeiro <alexdex12@hotmail.com>
2020-07-29 12:28:24 -0300

    Exceções do tabuleiro e programação defensiva

---
+++ src/JogoTabuleiro/ExcecaoTabuleiro.java
@@ -0,0 +1,12 @@
+package JogoTabuleiro;
+
+public class ExcecaoTabuleiro extends RuntimeException{
+
+	private static final long serialVersionUID = 1L;
+	
+	public ExcecaoTabuleiro(String msg) {
+		super(msg);
+	}
+	
+
+}
--- src/JogoTabuleiro/Tabuleiro.java
+++ src/JogoTabuleiro/Tabuleiro.java
@@ -8,6 +8,9 @@ public class Tabuleiro {
 	
 	
 	public Tabuleiro(int linhas, int colunas) {
+		if(linhas <1 || colunas < 1) {
+			throw new ExcecaoTabuleiro("Erro ao criar o tabuleiro:deve haver pelo menos 1 linha e 1 coluna");
+		}
 		
 		this.linhas = linhas;
 		this.colunas = colunas;
@@ -20,37 +23,53 @@ public class Tabuleiro {
 	}
 
 
-	public void setLinhas(int linhas) {
-		this.linhas = linhas;
-	}
-
-
 	public int getColunas() {
 		return colunas;
 	}
 
-
-	public void setColunas(int colunas) {
-		this.colunas = colunas;
-	}
-	
 	
 	//Retornar a peça pela linha e coluna
 	public Peca peca(int linha, int coluna) {
+		if(!posicaoExiste(linha, coluna)) {
+			throw new ExcecaoTabuleiro("Posição não existe no tabuleiro");
+		}
 		return pecas[linha][coluna];
 	}
 	
 	//Retornar a peça pela a posição
 	public Peca peca(Posicao posicao) {
+		if(!posicaoExiste(posicao)) {
+			throw new ExcecaoTabuleiro("Posição não existe no tabuleiro");
+		}
 		return pecas[posicao.getLinha()][posicao.getColuna()];
 	}
 	
 	public void ColocarPeca(Peca peca, Posicao posicao) {
+		if(ExisteAPeca(posicao)) {
+			throw new ExcecaoTabuleiro("já existe uma peça na posição" + posicao);
+		}
 		pecas[posicao.getLinha()][posicao.getColuna()] = peca;
 		peca.posicao = posicao;
 	}
 	
+	public boolean posicaoExiste(int linha, int coluna) {
+		return linha >= 0 && linha < linhas && coluna >= 0 && coluna < colunas;
+	}
 	
+	
+	//verifica se existe esta posiçao no tabuleiro
+	public boolean posicaoExiste (Posicao posicao) {
+		return posicaoExiste(posicao.getLinha(), posicao.getColuna());
+		
+	}
+	
+	//verificando se existe uma peca nesta posiçao
+	public boolean ExisteAPeca (Posicao posicao) {
+		if(!posicaoExiste(posicao)) {
+			throw new ExcecaoTabuleiro("Posição não existe no tabuleiro");
+		}
+	return peca(posicao) != null;
+	}
 	
 	
 	
